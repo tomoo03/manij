85a0eb393febf301afde4d129b39c4c7555a0715
t_omo_o_03 <shishimarureo@gmail.com>
2020-07-11 12:40:47 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'goals/new'
-  get 'goals/index'
-  get 'goals/show'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/help'
+  resources :goals
 end
