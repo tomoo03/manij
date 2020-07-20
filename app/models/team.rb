d0829f318e8dc82f6b110da55293f2592d6e4f58
t_omo_o_03 <shishimarureo@gmail.com>
2020-07-21 01:27:12 +0900

    Add viewport

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,5 @@ class Team < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
-  has_many :projects
+  has_many :projects, dependent: :destroy
 end
